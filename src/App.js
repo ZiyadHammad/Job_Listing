c805a5feec5099658c28db3bd502a772a1c006ef
Ziyad Hammad <ziyad.hammad@outlook.com>
2022-03-17 20:13:43 -0400

    Working on Search Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import { Box, ThemeProvider } from "@material-ui/core"
+import { ThemeProvider, Grid } from "@material-ui/core"
 import theme from "./theme/Themes"
 import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-     <Header /> 
+      <Header /> 
+      <Grid container justify="center">
+        <Grid item xs={10}>
+          <SearchBar />
+        </Grid>
+      </Grid>
     </ThemeProvider>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,17 @@
 import React from "react"
 import { Box, Grid, Typography, Button } from "@material-ui/core"
 
-export default (props) => (
-  <Box py={10} bgcolor="secondary.main" color="white" >
-    <Grid container justify="center">
-      <Grid item xs={10} >
-        <Box display="flex" justifyContent="space-between" >
-        <Typography variant="h5">Open Job Listing</Typography>
-          <Button variant="contained" color="primary" >Post a Job</Button>
+export default (props) => {
+  return (
+    <Box py={10} bgcolor="secondary.main" color="white" >
+      <Grid container justify="center">
+        <Grid item xs={10} >
+          <Box display="flex" justifyContent="space-between" >
+            <Typography variant="h3">Open Job Listing</Typography>
+            <Button variant="contained" color="primary" >Post a Job</Button>
           </Box>
+        </Grid>
       </Grid>
-     </Grid>
-  </Box>
-)
+    </Box>
+  )
+}
